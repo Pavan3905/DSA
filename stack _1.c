fcdb9ca3d59dbfac855564ea2e90374859d82c57
Pavan Kumar S <pavankumaryattinamane@gmail.com>
2026-02-19 23:16:38 +0530

    Refactor stack operations with improved messages

--- stack _1.c
+++ stack _1.c
@@ -8,13 +8,12 @@ void display(int top, int s[]);
 
 int main(void){
     int top, s[STACK_SIZE], item, item_deleted, choice;
-
     top = -1;
     for(;;){
-        printf("1. push");
-        printf("2. pop\n");
-        printf("3. display\n");
-        printf("4. exit\n\n");
+        printf("1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Display\n");
+        printf("4. Exit\n\n");
         printf("Enter the choice : ");
         scanf("%d", &choice);
 
@@ -27,17 +26,18 @@ int main(void){
             case 2:
                 item_deleted = pop(&top, s);
                 if(item_deleted == -1)
-                    printf("Stack is Empty\n");
+                    printf("[Stack Underflow]\n\n");
                 else
-                    printf("Item deleted = %d\n\n", item_deleted);
+                    printf("[Popped %d Succesfully]\n\n", item_deleted);
                 break;
             case 3:
                 display(top, s);
                 break;
             case 4:
+                printf("See You Soon (*_*)");
                 exit(0);
             default:
-                printf("Invalid Choice\n");
+                printf("[Invalid Choice]\n\n");
                 break;
         }
     }
@@ -46,12 +46,12 @@ int main(void){
 
 void push(int item, int* top, int s[]){
     if(*top == STACK_SIZE-1){
-        printf("Stack overflow\n");
-        return;
+        printf("[Stack Overflow]\n\n");
+    return;
     }
-    *top = *top+1;
+    *top = *top + 1;
     s[*top] = item;
-    printf("\n");
+    printf("[Pushed %d Succesfully]\n\n", s[*top]);
 }
 
 int pop(int* top, int s[]){
@@ -64,12 +64,12 @@ int pop(int* top, int s[]){
 
 void display(int top, int s[]){
     if(top == -1){
-        printf("Stack is Empty\n");
-        return;
+        printf("[Stack is Empty]\n\n");
+    return;
     }
-    printf("Contents of the stack\n");
+    printf("Overview Of Your Stack\n");
     printf("top --> ");
     for(int i = top; i>=0; i--)
         printf("%d ", s[i]);
-        printf("\n\n");
+    printf("\n\n");
 }
